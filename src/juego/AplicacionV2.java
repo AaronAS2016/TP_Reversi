0f993cbb70baf86f15765a238eb575057278fe3f
Aaron <aaronsaban98@gmail.com>
2018-11-04 14:53:29 -0300

    Primeras 2 pantalllas

--- src/juego/AplicacionV2.java
+++ src/juego/AplicacionV2.java
@@ -1,25 +1,52 @@
 package juego;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import  javafx.scene.control.Button;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
+import java.awt.*;
 import java.io.IOException;
 
 public class AplicacionV2 extends Application {
 
-
+    private Stage window;
+    private  Scene pantallaPrincipal;
+    private  Scene pantallaMenu;
 
     @Override
     public void start(Stage escenarioPrincipal) throws Exception {
 
-        Parent root =  FXMLLoader.load(getClass().getResource("./../views/pantalla-principal.fxml"));
+        window = escenarioPrincipal;
 
-        Scene escena = new Scene(root, 450, 600);
-        //btnIniciar.setOnAction(event -> checkID())
-        escenarioPrincipal.setScene(escena);
+        Parent pantallaPrincipalView =  FXMLLoader.load(getClass().getResource("./../views/pantalla-principal.fxml"));
+        Parent menu =  FXMLLoader.load(getClass().getResource("./../views/menu.fxml"));
+
+        pantallaPrincipal = new Scene(pantallaPrincipalView, 450, 600);
+        pantallaMenu = new Scene(menu, 450, 600);
+
+        Button btnIniciar = (Button) pantallaPrincipal.lookup("#btnIniciar");
+        btnIniciar.addEventHandler(MouseEvent.MOUSE_CLICKED, new MyEventHandler());
+
+
+        escenarioPrincipal.setScene(pantallaPrincipal);
         escenarioPrincipal.setTitle("REVERSI");
         escenarioPrincipal.show();
 
@@ -31,5 +58,12 @@ public class AplicacionV2 extends Application {
         Thread.setDefaultUncaughtExceptionHandler(new MostrarError());
 
         launch(args);
+    }
+
+    private class MyEventHandler implements EventHandler<Event> {
+        @Override
+        public void handle(Event evt) {
+            window.setScene(pantallaMenu);
+        }
     }
 }
