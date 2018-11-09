cca05d8ad0b7ea32775f07897e9a8c9c2abe1209
Aaron <aaronsaban98@gmail.com>
2018-11-09 13:46:33 -0300

    PInta Cruces en el tablero

--- src/juego/TableroController.java
+++ src/juego/TableroController.java
@@ -5,6 +5,9 @@ import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,9 +16,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TableroController{
@@ -43,9 +52,85 @@ public class TableroController{
     public void mostrar() throws IOException {
         Parent pantallaTablero =  FXMLLoader.load(getClass().getResource("./../views/tablero.fxml"));
         tablero = new Scene(pantallaTablero, 800,680);
+        tablero.getStylesheets().add(
+                getClass().getResource("/views/styles.css").toExternalForm());
         escenario.setScene(tablero);
         WindowStyle.allowDrag(pantallaTablero, escenario);
+
         crearControles();
+        dibujar();
+    }
+
+    public void dibujar() {
+
+        grilla.getChildren().clear();
+
+        for (int fila = 1; fila <= juego.contarFilas(); fila++) {
+
+            for (int columna = 1; columna <= juego.contarColumnas(); columna++) {
+
+                dibujarFicha(fila, columna);
+                //dibujarBoton(fila, columna);
+            }
+        }
+
+        dibujarEstado();
+    }
+
+    /**
+     * post: actualiza el estado del juego del en Tablero.
+     */
+    private void dibujarEstado() {
+
+        cantidadFichasNegras.setText(String.valueOf(juego.contarFichasNegras()));
+        cantidadFichasBlancas.setText(String.valueOf(juego.contarFichasBlancas()));
+        jugadorActual.setText(juego.obtenerJugadorActual());
+    }
+
+    /**
+     * post: dibuja la ficha en la posición indicada por fila y columna.
+     */
+    private void dibujarFicha(int fila, int columna){
+
+        Casillero casillero = juego.obtenerCasillero(fila, columna);
+
+
+        Image image = new Image("./img/cruz.png");
+        ImageView dibujoCasillero = new ImageView(image);
+        dibujoCasillero.setFitHeight(grilla.getWidth() / juego.contarColumnas() );
+        dibujoCasillero.setFitWidth(grilla.getHeight() / juego.contarFilas());
+
+        dibujoCasillero.getStyleClass().add("image-view-wrapper");
+
+        dibujar(dibujoCasillero, fila, columna);
+    }
+
+    /**
+     * post: dibuja el boton en el casillero indicado por fila y columna,
+     *       si es que se puede colocar una ficha.
+     *
+     * @param fila
+     * @param columna
+     */
+   /* private void dibujarBoton(int fila, int columna) {
+
+        if (juego.puedeColocarFicha(fila, columna)) {
+
+            Button botonColocarFicha = new Button();
+            botonColocarFicha.setMinSize(DIMENSION_BOTON, DIMENSION_BOTON);
+            botonColocarFicha.setMaxSize(DIMENSION_BOTON, DIMENSION_BOTON);
+            botonColocarFicha.setOnAction(new ColocarFicha(this, juego, fila, columna));
+
+            dibujar(botonColocarFicha, fila, columna);
+        }
+    }*/
+
+    private void dibujar(Node elemento, int fila, int columna) {
+
+        GridPane.setHalignment(elemento, HPos.CENTER);
+        GridPane.setValignment(elemento, VPos.CENTER);
+
+        grilla.add(elemento, columna - 1, fila - 1);
     }
 
     public void crearControles(){
@@ -63,11 +148,13 @@ public class TableroController{
         Label jugador1 = (Label) tablero.lookup("#txtJugador1");
         Label jugador2 = (Label) tablero.lookup("#txtJugador2");
 
-        jugador1.setText(juego.obtenerJugadores(1));
-        jugador2.setText(juego.obtenerJugadores(2));
+        jugador1.setText(juego.obtenerJugadores(1) + ":");
+        jugador2.setText(juego.obtenerJugadores(2) + ":");
 
         jugadorActual = (Label) tablero.lookup("#txtJugadorActual");
         jugadorActual.setText(juego.obtenerJugadorActual());
+
+        grilla = (GridPane) tablero.lookup("#grillaTablero");
     }
     public class Cerrar implements EventHandler<Event> {
         @Override
