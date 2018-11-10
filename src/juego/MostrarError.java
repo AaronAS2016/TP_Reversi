a9a4620d53340efcadb897c329fd6e6949cd0053
Aaron <aaronsaban98@gmail.com>
2018-11-10 06:34:30 -0300

    Mensaje de error creado

--- src/juego/MostrarError.java
+++ src/juego/MostrarError.java
@@ -1,12 +1,20 @@
 package juego;
 
+import java.io.IOException;
 import java.lang.Thread.UncaughtExceptionHandler;
 
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Captura errores en tiempo de ejecución y muestra una ventana con el detalle.
@@ -18,13 +26,28 @@ public class MostrarError implements UncaughtExceptionHandler {
 
 		Stage escenario = new Stage();
 		
-		Text mensaje = new Text(error.getMessage());
-		
-		BorderPane panel = new BorderPane();
-		panel.setPadding(new Insets(10,20,20,10));
-		panel.setCenter(mensaje);
+		String mensaje = new String(error.getMessage());
 
-		Scene escena = new Scene(panel);
+		Parent pantallaError = null;
+		try {
+			pantallaError = FXMLLoader.load(getClass().getResource("./../views/error.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Scene escena = new Scene(pantallaError, 450,130);
+		Button btnCerrar = (Button) pantallaError.lookup("#btnCerrar");
+		btnCerrar.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+			escenario.close();
+		});
+		Label errorText = (Label) pantallaError.lookup("#labelError");
+
+		errorText.setText(mensaje);
+
+		escenario.initStyle(StageStyle.UNDECORATED);
+		escenario.getIcons().add(new Image("./img/icono.png"));
+		WindowStyle.allowDrag(pantallaError, escenario);
+		WindowStyle.stageDimension(escenario.getWidth(), escenario.getHeight());
 
 		escenario.setScene(escena);
 		escenario.setResizable(false);
